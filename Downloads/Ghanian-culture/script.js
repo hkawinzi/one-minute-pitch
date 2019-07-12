81fd025a6b3d966aa4194dc0b2792091e7ec84c8
happiness <happinesskawinzi@gmail.com>
2019-07-12 12:24:07 +0300

    addition of text

--- Downloads/Ghanian-culture/script.js
+++ Downloads/Ghanian-culture/script.js
@@ -14,3 +14,19 @@ alert ("Kofi");
 } else (dayName == "Saturday"); {
 alert ("Kwame")
 }
+var answerfemale = prompt("enter dayName");
+if (dayName == "Sunday") {
+alert ("Akosua");
+} else if (dayName == "Monday") {
+alert ("Adwoa");
+} else if (dayName == "Tuesday") {
+alert ("Abenaa");
+} else if (dayName == "Wednesday") {
+alert ("Akua");
+} else if (dayName == "Thursday") {
+alert ("Yaa");
+} else if (dayName == "Friday") {
+alert ("Afua");
+} else (dayName == "Saturday"); {
+alert ("Ama")
+}
