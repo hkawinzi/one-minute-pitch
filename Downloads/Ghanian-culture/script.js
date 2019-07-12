5b7b57600813e1b8ec3767db940791468e3679a4
happiness <happinesskawinzi@gmail.com>
2019-07-12 11:27:45 +0300

    dayName

---
+++ Downloads/Ghanian-culture/script.js
@@ -0,0 +1,16 @@
+var dayName =prompt ("enter day name");
+if (dayName == "Sunday") {
+alert ("Kwasi");
+} else if (dayName == "Monday") {
+alert ("kwadwo");
+} else if (dayName == "Tuesday") {
+alert ("Kwabena");
+} else if (dayName == "Wednesday") {
+alert ("Kwaku");
+} else if (dayName == "Thursday") {
+alert ("Yaw");
+} else if (dayName == "Friday") {
+alert ("Kofi");
+} else (dayName == "Saturday"); {
+alert ("Kwame")
+}
