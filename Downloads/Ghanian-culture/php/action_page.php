c248680c9b9efe2d3a8836f265bb23ecf3344d24
happiness <happinesskawinzi@gmail.com>
2019-07-13 14:08:22 +0300

    page

---
+++ Downloads/Ghanian-culture/php/action_page.php
@@ -0,0 +1,14 @@
+function getInputs () {
+var century = parseInt(document.getElementByID("century"));
+var year = parseInt(document.getElementByID("year"));
+//month
+//date/day of the month
+}
+
+//calculate day of the week
+function calculate(){
+get inputs();
+}
+var day = Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
+return (Math.floor(day);
+}
