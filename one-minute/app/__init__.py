1e030537cfb82a2d42a5d80b2ea3f3ea35fb93b2
happiness <happinesskawinzi@gmail.com>
2019-09-17 07:59:21 +0300

    creating a blueprint

--- one-minute/app/__init__.py
+++ one-minute/app/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
     db.init_app(app)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Regestering the auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/')
