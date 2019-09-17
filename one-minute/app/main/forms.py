1cf80f1f5336f16c9558fc4b3f05d058f2227204
happiness <happinesskawinzi@gmail.com>
2019-09-17 08:26:16 +0300

    importing modules from wtf forms

--- one-minute/app/main/forms.py
+++ one-minute/app/main/forms.py
@@ -1,3 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField, StringField
+from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     pitch = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
@@ -22,4 +25,22 @@ class ProductionForm(FlaskForm):
 class InterviewForm(FlaskForm):
     post = StringField('Title', validators=[Required()])
     body = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+class PromotionCommentForm(FlaskForm):
+    comment = StringField('post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class PickCommentForm(FlaskForm):
+    comment = StringField('post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class ProductionCommentForm(FlaskForm):
+    comment = StringField('post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class InterviewCommentForm(FlaskForm):
+    comment = StringField('post', validators=[Required()])
     submit = SubmitField('Submit')
